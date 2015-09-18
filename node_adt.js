c81156ff14502628469709d0d71c6ecb014c5e3e
Albert Zak <me@albertzak.com>
2015-09-18 17:58:51 +0200

    Add header parsing

--- node_adt.js
+++ node_adt.js
@@ -1,6 +1,20 @@
+var fs = require('fs');
+
 var Adt = function() {
-  this.hello = function() {
-    return 'hello world';
+
+  this.open = function(path) {
+    this.data = fs.readFileSync(path);
+    return this.parseHeader();
+  }
+
+  this.parseHeader = function() {
+    var header = {};
+
+    header.recordCount  = this.data.readUIntLE(24);
+    header.columnCount  = this.data.readUIntLE(33);
+    header.recordLength = this.data.readUIntLE(36);
+
+    return header;
   }
 };
 
--- test/test.js
+++ test/test.js
@@ -1,10 +1,19 @@
-var adt = require('../node_adt.js');
+var Adt = require('../node_adt.js');
 var assert = require('assert');
 
 describe('Adt', function() {
-  describe('.hello', function () {
-    it('should return "hello world"', function () {
-      assert.equal(adt.hello(), 'hello world');
+  describe('.open', function () {
+    it('should not throw when path exists', function () {
+      assert.doesNotThrow(function() { Adt.open('./test/fixtures/ARZTSTAT474.adt')});
+    });
+
+    it('should throw when path does not exist', function () {
+      assert.throws(function() { Adt.open('./test/invalid')});
+    });
+
+    it('should have a column count', function () {
+      console.log(Adt.open('./test/fixtures/ARZTSTAT474.adt'));
+      assert(true);
     });
   });
 });
