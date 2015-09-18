271104bfb42c989926df6ab79d805021645909a2
Albert Zak <me@albertzak.com>
2015-09-18 19:01:43 +0200

    Add column name parsing

--- node_adt.js
+++ node_adt.js
@@ -2,9 +2,15 @@ var fs = require('fs');
 
 var Adt = function() {
 
+  var HEADER_LENGTH = 400,
+      COLUMN_LENGTH = 200;
+
   this.open = function(path) {
     this.data = fs.readFileSync(path);
-    return this.parseHeader();
+    this.header = this.parseHeader();
+    this.columns = this.parseColumns();
+    
+    return this;
   }
 
   // Determine record count, column count, record length, and data offset
@@ -18,6 +24,21 @@ var Adt = function() {
 
     return header;
   }
+
+  // Retrieves column information from the database
+  this.parseColumns = function() {
+    var columns = [];
+
+    for(var i=0; i < this.header.columnCount; i++) {
+      var column = this.data.slice(HEADER_LENGTH + COLUMN_LENGTH * i);
+
+      var name = column.toString('ascii', 0, 128).trim().replace(/\0/g, '');
+      columns.push({name: name});
+    }
+
+    return columns;
+  }
+
 };
 
 module.exports = new Adt();
--- test/test.js
+++ test/test.js
@@ -16,23 +16,45 @@ describe('Adt', function() {
     var header = {};
 
     beforeEach(function() {
-      header = Adt.open('./test/fixtures/ARZTSTAT474.adt');
+      header = Adt.open('./test/fixtures/ARZTSTAT474.adt').header;
     });
 
-    it('should get the correct record count', function () {
+    it('should get the record count', function () {
       assert.equal(header.recordCount, 139);
     });
 
-    it('should get the correct column count', function () {
+    it('should get the column count', function () {
       assert.equal(header.columnCount, 11);
     });
 
-    it('should get the correct record length', function () {
+    it('should get the record length', function () {
       assert.equal(header.recordLength, 365);
     });
 
-    it('should get the correct data offset', function () {
+    it('should get the data offset', function () {
       assert.equal(header.dataOffset, 2600);
+    });
+  });
+
+  describe('#parseColumns', function() {
+    var columns = {};
+
+    beforeEach(function() {
+      columns = Adt.open('./test/fixtures/ARZTSTAT474.adt').columns;
+    });
+
+    it('should get the names for all columns', function () {
+      assert.equal(columns[0].name, 'Id');
+      assert.equal(columns[1].name, 'PatId');
+      assert.equal(columns[2].name, 'Datum');
+      assert.equal(columns[3].name, 'Kurzz');
+      assert.equal(columns[4].name, 'Text');
+      assert.equal(columns[5].name, 'Info');
+      assert.equal(columns[6].name, 'TextColor');
+      assert.equal(columns[7].name, 'BackColor');
+      assert.equal(columns[8].name, 'InfTextCol');
+      assert.equal(columns[9].name, 'InfBackCol');
+      assert.equal(columns[10].name, 'CodeInfo');
     });
   });
 
